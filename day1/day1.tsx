211d7535b1d15130c379d85db35b7481ca0ea443
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-12-01 19:32:02 +0100

    feat: day 1 part 2

--- day1/day1.tsx
+++ day1/day1.tsx
@@ -5,7 +5,19 @@ const input = readFileSync("./input.txt").toString("utf8").split("\n").filter(s 
 const left = input.map(s => s.split("   ")[0]).map(s => Number(s)).sort((a, b) => a - b)
 const right = input.map(s => s.split("   ")[1]).map(s => Number(s)).sort((a, b) => a - b)
 
+// PART 1
 const result = left.reduce((acc, curr, index) => acc + Math.abs(curr - right[index]), 0)
-console.log(result)
+
+//PART 2
+const result2 = left.reduce((acc, curr) => {
+  const matches = right.filter(s => s == curr).length
+  if (matches == 0) {
+    return acc
+  }
+  return acc + (curr * matches)
+}, 0)
+
+
+console.log(`Day 1, part 1: ${result}, part 2: ${result2}`)
 
 
